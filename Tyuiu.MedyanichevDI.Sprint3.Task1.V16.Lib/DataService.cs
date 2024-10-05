601f329d1d0bb5aa5167365ca96b89cea34d78c9
Денис <121244780+Domovoy23@users.noreply.github.com>
2024-10-05 12:10:13 +0500

    Task1

--- Tyuiu.MedyanichevDI.Sprint3.Task1.V16.Lib/DataService.cs
+++ Tyuiu.MedyanichevDI.Sprint3.Task1.V16.Lib/DataService.cs
@@ -3,10 +3,10 @@ namespace Tyuiu.MedyanichevDI.Sprint3.Task1.V16.Lib
 {
     public class DataService : ISprint3Task1V16
     {
-        double res = 1;
+        double res = 0;
         public double GetSumSeries(double value, int startValue, int stopValue)
         {
-            while (startValue!= stopValue)
+            while (startValue<= stopValue)
             {
                 res += Math.Pow(value, 2) * Math.Sin(startValue) + 1;
                 startValue++;
--- Tyuiu.MedyanichevDI.Sprint3.Task1.V16.Test/DataServiceTest.cs
+++ Tyuiu.MedyanichevDI.Sprint3.Task1.V16.Test/DataServiceTest.cs
@@ -1,3 +1,5 @@
+using Tyuiu.MedyanichevDI.Sprint3.Task1.V16.Lib;
+
 namespace Tyuiu.MedyanichevDI.Sprint3.Task1.V16.Test
 {
     [TestClass]
@@ -6,6 +8,11 @@ namespace Tyuiu.MedyanichevDI.Sprint3.Task1.V16.Test
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new DataService();
+            double x =0.7;
+            int s = 1;
+            int e = 15;
+            Assert.AreEqual(15.948, ds.GetSumSeries(x, s, e));
         }
     }
 }
