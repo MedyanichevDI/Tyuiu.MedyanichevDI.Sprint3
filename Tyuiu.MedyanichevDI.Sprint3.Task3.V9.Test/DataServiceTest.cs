5aa0c8d84476484ce9b6e5621314baf896c8272b
Денис <121244780+Domovoy23@users.noreply.github.com>
2024-10-07 20:34:40 +0500

    Task3

---
+++ Tyuiu.MedyanichevDI.Sprint3.Task3.V9.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.MedyanichevDI.Sprint3.Task3.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.MedyanichevDI.Sprint3.Task3.V9/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using Tyuiu.MedyanichevDI.Sprint3.Task3.V9.Lib;
+namespace Tyuiu.MedyanichevDI.Sprint3.Task3.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Медяничев Д.И. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Медяничев Денис Игоревич | АСОиУб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("Написать программу используя цикл for, которая вычисляет произведение     *");
+            Console.WriteLine("*ряда по формуле, при x = 0,7                                             *");
+            // Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("                                                                          *");
+            Console.WriteLine("***************************************************************************");
+
+            //string k;
+
+            Console.WriteLine("Введите значение: ");
+            string x = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите букву: ");
+            char y = Convert.ToChar(Console.ReadLine());
+           
+
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.ReplaceNumOnChar(x, y));
+
+            Console.ReadKey();
+        }
+    }
+}
